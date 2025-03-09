894b0a8fb273e3ebfa359acea2f66e3e4d074f7b
Carlos <carlos_buske@hotmail.com>
2025-03-09 16:51:26 -0300

    criando novas rotas (post,get,delete,put)

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,6 +1,8 @@
 import { DataSource } from "typeorm";
 import { createUser } from "../model/users";
 import { Request, Response } from "express";
+import { AppDataSource } from "../data-source/database-conection";
+import { User } from "../entity/entity";
 
 export async function userPost(req: Request, res: Response) {
   const { name, user_name, email, avatar, bio, gender, password_hash } =
@@ -18,5 +20,30 @@ export async function userPost(req: Request, res: Response) {
     res.status(201).send("user created");
   } catch (Erorr) {
     res.send(404).send("erro");
+  }
+}
+export async function userGetAll(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const getRepository = AppDataSource.getRepository(User);
+    const getAll = await getRepository.find();
+    res.status(200).json(getAll);
+  } catch (Error) {
+    res.status(404).send("nenhum usuário encontrado");
+  }
+}
+export async function userGetOne(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const getrepository = AppDataSource.getRepository(User);
+    const getOne = await getrepository.findOne({
+      where: { id: Number(id) },
+    });
+    if (!getOne) {
+      res.status(404).send("usuário não foi encontrado");
+    }
+    res.status(200).json(getOne);
+  } catch (Error) {
+    res.status(500).send("erro de comunicação");
   }
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
-import { userPost } from "../controllers/controller";
-const app = express();
+import { userGetAll, userGetOne, userPost } from "../controllers/controller";
+
 export const routes = express.Router();
 
 routes.post("/user", userPost);
+routes.get("/user", userGetAll);
+routes.get("/user/:id", userGetOne);
 
 export default routes;
