4ac71d3f904c3e51008a756f24718e4945378921
Carlos <carlos_buske@hotmail.com>
2025-03-09 17:40:41 -0300

    criando classe dos controllers

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -4,46 +4,86 @@ import { Request, Response } from "express";
 import { AppDataSource } from "../data-source/database-conection";
 import { User } from "../entity/entity";
 
-export async function userPost(req: Request, res: Response) {
-  const { name, user_name, email, avatar, bio, gender, password_hash } =
-    req.body;
-  try {
-    await createUser(
-      name,
-      user_name,
-      email,
-      avatar,
-      bio,
-      gender,
-      password_hash
-    );
-    res.status(201).send("user created");
-  } catch (Erorr) {
-    res.send(404).send("erro");
-  }
-}
-export async function userGetAll(req: Request, res: Response) {
-  const { id } = req.params;
-  try {
-    const getRepository = AppDataSource.getRepository(User);
-    const getAll = await getRepository.find();
-    res.status(200).json(getAll);
-  } catch (Error) {
-    res.status(404).send("nenhum usuário encontrado");
-  }
-}
-export async function userGetOne(req: Request, res: Response) {
-  const { id } = req.params;
-  try {
-    const getrepository = AppDataSource.getRepository(User);
-    const getOne = await getrepository.findOne({
-      where: { id: Number(id) },
-    });
-    if (!getOne) {
-      res.status(404).send("usuário não foi encontrado");
+export class UserController {
+  async userCreate(req: Request, res: Response) {
+    const { name, user_name, email, avatar, bio, gender, password_hash } =
+      req.body;
+    try {
+      await createUser(
+        name,
+        user_name,
+        email,
+        avatar,
+        bio,
+        gender,
+        password_hash
+      );
+      res.status(201).send("user created");
+    } catch (Error) {
+      res.send(404).send("erro");
     }
-    res.status(200).json(getOne);
-  } catch (Error) {
-    res.status(500).send("erro de comunicação");
+  }
+  async userGetAll(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const getRepository = AppDataSource.getRepository(User);
+      const getAll = await getRepository.find();
+      res.status(200).json(getAll);
+    } catch (Error) {
+      res.status(404).send("nenhum usuário encontrado");
+    }
+  }
+  async userGetOne(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const getrepository = AppDataSource.getRepository(User);
+      const getOne = await getrepository.findOne({
+        where: { id: Number(id) },
+      });
+      if (!getOne) {
+        res.status(404).send("usuário não foi encontrado");
+      }
+      res.status(200).json(getOne);
+    } catch (Error) {
+      res.status(500).send("erro de comunicação");
+    }
+  }
+  async userDelete(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const getRepository = AppDataSource.getRepository(User);
+      const getToDelete = await getRepository.findOne({
+        where: { id: Number(id) },
+      });
+      if (!getToDelete) {
+        res.status(404).send("o usuário não foi localizado na base de dados");
+      }
+      await getRepository.delete(id);
+      res.status(200).send("usuário deletado da base de dados");
+    } catch (Error) {
+      res.status(500).send("erro inesperado");
+    }
+  }
+  async userModify(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const userRepository = AppDataSource.getRepository(User);
+      const userModify = await userRepository.findOne({
+        where: { id: Number(id) },
+      });
+      if (userModify) {
+        userModify.name = req.body.name;
+        userModify.user_name = req.body.user_name;
+        userModify.email = req.body.email;
+        userModify.avatar = req.body.avatar;
+        userModify.bio = req.body.bio;
+        userModify.gender = req.body.gender;
+        userModify.password_hash = req.body.password_hash;
+        await userRepository.save(userModify);
+      }
+      res.status(200).send("usuário modificado com sucesso!");
+    } catch (Error) {
+      res.status(404).send("não foi possível alterar o usuário");
+    }
   }
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express";
-import { userGetAll, userGetOne, userPost } from "../controllers/controller";
+import { UserController } from "../controllers/controller";
 
 export const routes = express.Router();
+const userControllers = new UserController();
 
-routes.post("/user", userPost);
-routes.get("/user", userGetAll);
-routes.get("/user/:id", userGetOne);
+routes.post("/user", userControllers.userCreate);
+routes.get("/user", userControllers.userGetAll);
+routes.get("/user/:id", userControllers.userGetOne);
+routes.delete("/user/:id", userControllers.userDelete);
+routes.put("/user/:id", userControllers.userModify);
 
 export default routes;
